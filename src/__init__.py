ba76a6d44ebe74a552f7c21013c4da471931abb5
calyxish <kwakyeishmael07@gmail.com>
2026-05-15 17:44:02 +0000

    feat: add simulation utilities
    Closes #2

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+neuronal-stability-analysis
+---------------------------
+Author : Kwakye Ishmael Affum
+GitHub : https://github.com/calyxish
+"""
+from .simulation import simulate, stability_info
+
+__all__ = ["simulate", "stability_info"]
